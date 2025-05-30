1a685260d2fb2974945e8d6ce60f908b21b3388c
ksh <kshman@msn.com>
2025-05-30 09:33:16 +0900

    Cosmic & South Horn (#219)
    * Cosmic & South Horn
    
    * Occult Crescent: South Horn

--- DcConfig.cs
+++ DcConfig.cs
@@ -12,7 +12,7 @@ namespace DutyContent
 {
 	class DcConfig
 	{
-		public static int PluginTag => 34;
+		public static int PluginTag => 35;
 		public static Version PluginVersion => System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
 
 		public static bool PluginEnable { get; set; }
